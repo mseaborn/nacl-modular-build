ec25695048e8686f34fb110a017c27cf745d8789
mseaborn <mseaborn@4d43a764-8522-4e11-915f-eb4c4899fa76>
2009-12-07 16:41:56 +0000

    Add newlib build  * Factor out common code
    git-svn-id: file:///home/mseaborn/svn/scratch/newtools@4 4d43a764-8522-4e11-915f-eb4c4899fa76

--- build.py
+++ build.py
@@ -6,20 +6,25 @@ import action_tree
 import cmd_env
 
 
+nacl_dir = "/home/mseaborn/devel/nacl-trunk/src/native_client"
 tar_dir = "/home/mseaborn/devel/nacl-trunk/src/third_party"
 patch_dir = "/home/mseaborn/devel/nacl-trunk/src/native_client/tools/patches"
 
 
-class Module1(object):
+class ModuleBase(object):
 
-    def __init__(self):
-        self._name = "binutils"
+    def __init__(self, prefix):
         self._env = cmd_env.VerboseWrapper(cmd_env.BasicEnv())
-        self._prefix = os.path.join(os.getcwd(), "prefix")
+        self._prefix = prefix
 
     def all(self):
         return action_tree.make_node(
-            [self.unpack, self.configure, self.make], self._name)
+            [self.unpack, self.configure, self.make], self.name)
+
+
+class Module1(ModuleBase):
+
+    name = "binutils"
 
     def unpack(self, log):
         self._env.cmd(["mkdir", "-p", "source"])
@@ -44,16 +49,9 @@ class Module1(object):
         self._env.cmd(["sh", "-c", "cd build/binutils && make install"])
 
 
-class Module2(object):
+class Module2(ModuleBase):
 
-    def __init__(self):
-        self._name = "gcc"
-        self._env = cmd_env.VerboseWrapper(cmd_env.BasicEnv())
-        self._prefix = os.path.join(os.getcwd(), "prefix")
-
-    def all(self):
-        return action_tree.make_node(
-            [self.unpack, self.configure, self.make], self._name)
+    name = "gcc"
 
     def unpack(self, log):
         self._env.cmd(["mkdir", "-p", "source"])
@@ -95,13 +93,62 @@ class Module2(object):
         self._env.cmd(["sh", "-c", "cd build/gcc && make install-gcc"])
 
 
+class Module3(ModuleBase):
+
+    name = "newlib"
+
+    def unpack(self, log):
+        self._env.cmd(["mkdir", "-p", "source"])
+        self._env.cmd(["tar", "-C", "source", "-xf",
+                       os.path.join(tar_dir, "newlib/newlib-1.17.0.tar.gz")])
+        self._env.cmd(["patch", "-d", "source/newlib-1.17.0",
+                       "-p1",
+                       "-i", os.path.join(patch_dir, "newlib-1.17.0.patch")])
+
+    def configure(self, log):
+        # This is like exporting the kernel headers to glibc.
+        # This should be done differently.
+        self._env.cmd(
+            [os.path.join(nacl_dir, "src/trusted/service_runtime/export_header.py"),
+             os.path.join(nacl_dir, "src/trusted/service_runtime/include"),
+             "source/newlib-1.17.0/newlib/libc/sys/nacl"])
+
+        self._env.cmd(["mkdir", "-p", "build/newlib"])
+        # CFLAGS has to be passed via environment because the
+        # configure script can't cope with spaces otherwise.
+        self._env.cmd(["sh", "-c",
+                       "cd build/newlib && "
+                       'CFLAGS="-m32 -march=i486 -msse2 -mfpmath=sse" '
+                       "../../source/newlib-1.17.0/configure "
+                       "--enable-newlib-io-long-long "
+                       "--enable-newlib-io-c99-formats "
+                       "--prefix=%s "
+                       "--target=nacl"
+                       % self._prefix])
+
+    def make(self, log):
+        self._env.cmd(["sh", "-c", "cd build/newlib && make"])
+        self._env.cmd(["sh", "-c", "cd build/newlib && make install"])
+
+
 mods = [
-    Module1(),
-    Module2(),
+    Module1,
+    Module2,
+    Module3,
     ]
 
 def all_mods():
-    return action_tree.make_node([mod.all() for mod in mods], name="all")
+    nodes = []
+    env = cmd_env.VerboseWrapper(cmd_env.BasicEnv())
+    prefix_base = os.path.join(os.getcwd(), "prefix")
+    for mod in mods:
+        # prefix = os.path.join(prefix_base, mod.name)
+        # os.environ["PATH"] = "%s:%s" % (prefix, os.environ["PATH"])
+        prefix = os.path.join(os.getcwd(), "sharedprefix")
+        os.environ["PATH"] = "%s:%s" % (os.path.join(prefix, "bin"),
+                                        os.environ["PATH"])
+        nodes.append(mod(prefix).all())
+    return action_tree.make_node(nodes, name="all")
 
 
 if __name__ == "__main__":
