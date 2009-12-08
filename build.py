43eca9985a98ddb8aa1d53f6aa3aa36cc3d30000
mseaborn <mseaborn@4d43a764-8522-4e11-915f-eb4c4899fa76>
2009-12-08 12:21:57 +0000

    Make unpack incremental: don't unpack if source tree exists  * Factor out source_dir and build_dir
    git-svn-id: file:///home/mseaborn/svn/scratch/newtools@5 4d43a764-8522-4e11-915f-eb4c4899fa76

--- build.py
+++ build.py
@@ -11,67 +11,99 @@ tar_dir = "/home/mseaborn/devel/nacl-trunk/src/third_party"
 patch_dir = "/home/mseaborn/devel/nacl-trunk/src/native_client/tools/patches"
 
 
+def get_one(lst):
+    assert len(lst) == 1, lst
+    return lst[0]
+
+
+def untar_multiple(env, dest_dir, tar_files):
+    assert os.listdir(dest_dir) == []
+    for tar_file in tar_files:
+        env.cmd(["tar", "-C", dest_dir, "-xf", tar_file])
+    tar_name = get_one(os.listdir(dest_dir))
+    for leafname in os.listdir(os.path.join(dest_dir, tar_name)):
+        os.rename(os.path.join(dest_dir, tar_name, leafname),
+                  os.path.join(dest_dir, leafname))
+    os.rmdir(os.path.join(dest_dir, tar_name))
+
+
+def untar(env, dest_dir, tar_file):
+    assert os.listdir(dest_dir) == []
+    env.cmd(["tar", "-C", dest_dir, "-xf", tar_file])
+    tar_name = get_one(os.listdir(dest_dir))
+    for leafname in os.listdir(os.path.join(dest_dir, tar_name)):
+        os.rename(os.path.join(dest_dir, tar_name, leafname),
+                  os.path.join(dest_dir, leafname))
+    os.rmdir(os.path.join(dest_dir, tar_name))
+
+
 class ModuleBase(object):
 
     def __init__(self, prefix):
         self._env = cmd_env.VerboseWrapper(cmd_env.BasicEnv())
+        self._source_dir = os.path.join(os.getcwd(), "source", self.name)
+        self._build_dir = os.path.join(os.getcwd(), "build", self.name)
         self._prefix = prefix
+        self._build_env = cmd_env.PrefixCmdEnv(
+            cmd_env.in_dir(self._build_dir), self._env)
+        self._args = {"prefix": self._prefix,
+                      "source_dir": self._source_dir}
 
     def all(self):
         return action_tree.make_node(
             [self.unpack, self.configure, self.make], self.name)
+
+    def unpack(self, log):
+        if not os.path.exists(self._source_dir):
+            temp_dir = "%s.temp" % self._source_dir
+            os.mkdir(temp_dir)
+            self.get(self._env, temp_dir)
+            os.rename(temp_dir, self._source_dir)
 
 
 class Module1(ModuleBase):
 
     name = "binutils"
 
-    def unpack(self, log):
-        self._env.cmd(["mkdir", "-p", "source"])
-        self._env.cmd(["tar", "-C", "source", "-xf",
-                       os.path.join(tar_dir, "binutils/binutils-2.20.tar.bz2")])
-        self._env.cmd(["patch", "-d", "source/binutils-2.20",
-                       "-p1",
+    def get(self, env, dest_dir):
+        untar(env, dest_dir,
+              os.path.join(tar_dir, "binutils/binutils-2.20.tar.bz2"))
+        self._env.cmd(["patch", "-d", dest_dir, "-p1",
                        "-i", os.path.join(patch_dir, "binutils-2.20.patch")])
 
     def configure(self, log):
-        self._env.cmd(["mkdir", "-p", "build/binutils"])
-        self._env.cmd(["sh", "-c",
-                       "cd build/binutils && "
-                       "../../source/binutils-2.20/configure "
+        self._env.cmd(["mkdir", "-p", self._build_dir])
+        self._build_env.cmd(["sh", "-c",
+                       "%(source_dir)s/configure "
                        'CFLAGS="-DNACL_ALIGN_BYTES=32 -DNACL_ALIGN_POW2=5" '
-                       "--prefix=%s "
+                       "--prefix=%(prefix)s "
                        "--target=nacl"
-                       % self._prefix])
+                       % self._args])
 
     def make(self, log):
-        self._env.cmd(["sh", "-c", "cd build/binutils && make"])
-        self._env.cmd(["sh", "-c", "cd build/binutils && make install"])
+        self._build_env.cmd(["make"])
+        self._build_env.cmd(["make", "install"])
 
 
 class Module2(ModuleBase):
 
     name = "gcc"
 
-    def unpack(self, log):
-        self._env.cmd(["mkdir", "-p", "source"])
-        self._env.cmd(["tar", "-C", "source", "-xf",
-                       os.path.join(tar_dir, "gcc/gcc-core-4.2.2.tar.bz2")])
-        self._env.cmd(["tar", "-C", "source", "-xf",
-                       os.path.join(tar_dir, "gcc/gcc-g++-4.2.2.tar.bz2")])
-        self._env.cmd(["patch", "-d", "source/gcc-4.2.2",
-                       "-p1",
+    def get(self, env, dest_dir):
+        untar_multiple(env, dest_dir,
+                       [os.path.join(tar_dir, "gcc/gcc-core-4.2.2.tar.bz2"),
+                        os.path.join(tar_dir, "gcc/gcc-g++-4.2.2.tar.bz2")])
+        self._env.cmd(["patch", "-d", dest_dir, "-p1",
                        "-i", os.path.join(patch_dir, "gcc-4.2.2.patch")])
 
     def configure(self, log):
-        self._env.cmd(["mkdir", "-p", "build/gcc"])
+        self._env.cmd(["mkdir", "-p", self._build_dir])
         # CFLAGS has to be passed via environment because the
         # configure script can't cope with spaces otherwise.
-        self._env.cmd(["sh", "-c",
-                       "cd build/gcc && "
+        self._build_env.cmd(["sh", "-c",
                        "CC=gcc "
                        'CFLAGS="-Dinhibit_libc -D__gthr_posix_h -DNACL_ALIGN_BYTES=32 -DNACL_ALIGN_POW2=5" '
-                       "../../source/gcc-4.2.2/configure "
+                       "%(source_dir)s/configure "
 
                        "--without-headers "
                        "--disable-libmudflap "
@@ -82,27 +114,25 @@ class Module2(ModuleBase):
                        "--disable-libstdcxx-pch "
                        "--disable-shared "
 
-                       "--prefix=%s "
+                       "--prefix=%(prefix)s "
                        "--target=nacl"
-                       % self._prefix])
+                       % self._args])
 
     def make(self, log):
         # The default make target doesn't work - it gives libiberty
         # configure failures.  Need to do "all-gcc" instead.
-        self._env.cmd(["sh", "-c", "cd build/gcc && make all-gcc -j2"])
-        self._env.cmd(["sh", "-c", "cd build/gcc && make install-gcc"])
+        self._build_env.cmd(["sh", "-c", "make all-gcc -j2"])
+        self._build_env.cmd(["sh", "-c", "make install-gcc"])
 
 
 class Module3(ModuleBase):
 
     name = "newlib"
 
-    def unpack(self, log):
-        self._env.cmd(["mkdir", "-p", "source"])
-        self._env.cmd(["tar", "-C", "source", "-xf",
-                       os.path.join(tar_dir, "newlib/newlib-1.17.0.tar.gz")])
-        self._env.cmd(["patch", "-d", "source/newlib-1.17.0",
-                       "-p1",
+    def get(self, env, dest_dir):
+        untar(env, dest_dir,
+              os.path.join(tar_dir, "newlib/newlib-1.17.0.tar.gz"))
+        self._env.cmd(["patch", "-d", dest_dir, "-p1",
                        "-i", os.path.join(patch_dir, "newlib-1.17.0.patch")])
 
     def configure(self, log):
@@ -111,24 +141,23 @@ class Module3(ModuleBase):
         self._env.cmd(
             [os.path.join(nacl_dir, "src/trusted/service_runtime/export_header.py"),
              os.path.join(nacl_dir, "src/trusted/service_runtime/include"),
-             "source/newlib-1.17.0/newlib/libc/sys/nacl"])
+             os.path.join(self._source_dir, "newlib/libc/sys/nacl")])
 
-        self._env.cmd(["mkdir", "-p", "build/newlib"])
+        self._env.cmd(["mkdir", "-p", self._build_dir])
         # CFLAGS has to be passed via environment because the
         # configure script can't cope with spaces otherwise.
-        self._env.cmd(["sh", "-c",
-                       "cd build/newlib && "
+        self._build_env.cmd(["sh", "-c",
                        'CFLAGS="-m32 -march=i486 -msse2 -mfpmath=sse" '
-                       "../../source/newlib-1.17.0/configure "
+                       "%(source_dir)s/configure "
                        "--enable-newlib-io-long-long "
                        "--enable-newlib-io-c99-formats "
-                       "--prefix=%s "
+                       "--prefix=%(prefix)s "
                        "--target=nacl"
-                       % self._prefix])
+                       % self._args])
 
     def make(self, log):
-        self._env.cmd(["sh", "-c", "cd build/newlib && make"])
-        self._env.cmd(["sh", "-c", "cd build/newlib && make install"])
+        self._build_env.cmd(["sh", "-c", "make"])
+        self._build_env.cmd(["sh", "-c", "make install"])
 
 
 mods = [
---
+++ build_test.py
@@ -0,0 +1,61 @@
+
+import os
+import shutil
+import subprocess
+import tempfile
+import unittest
+
+import build
+import cmd_env
+
+
+def read_file(filename):
+    fh = open(filename, "r")
+    try:
+        return fh.read()
+    finally:
+        fh.close()
+
+def write_file(filename, data):
+    fh = open(filename, "w")
+    try:
+        fh.write(data)
+    finally:
+        fh.close()
+
+
+# From http://lackingrhoticity.blogspot.com/2008/11/tempdirtestcase-python-unittest-helper.html
+class TempDirTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self._on_teardown = []
+
+    def make_temp_dir(self):
+        temp_dir = tempfile.mkdtemp(prefix="tmp-%s-" % self.__class__.__name__)
+        def tear_down():
+            shutil.rmtree(temp_dir)
+        self._on_teardown.append(tear_down)
+        return temp_dir
+
+    def tearDown(self):
+        for func in reversed(self._on_teardown):
+            func()
+
+
+class Test(TempDirTestCase):
+
+    def test_untar(self):
+        temp_dir = self.make_temp_dir()
+        os.mkdir(os.path.join(temp_dir, "foo-1.0"))
+        write_file(os.path.join(temp_dir, "foo-1.0", "README"), "hello")
+        tar_file = os.path.join(temp_dir, "foo-1.0.tar.gz")
+        subprocess.check_call(["tar", "-cf", tar_file, "foo-1.0"],
+                              cwd=temp_dir)
+
+        dest_dir = self.make_temp_dir()
+        build.untar(cmd_env.BasicEnv(), dest_dir, tar_file)
+        self.assertEquals(os.listdir(dest_dir), ["README"])
+
+
+if __name__ == "__main__":
+    unittest.main()
